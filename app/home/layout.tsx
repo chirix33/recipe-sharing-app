7b3957d8ad2390c103056558e0c6c81690c38e03
chirix33 <chirixteam@gmail.com>
2024-09-30 15:59:39 -0500

    Update middleware logic

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,8 +1,9 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
+      // Nav for all the Recipe Pages
       <div className="flex flex-col h-screen">
         <header className="bg-blue-500 text-white p-4">
-          <h1 className="text-2xl">Home</h1>
+          <h1 className="text-2xl">Recipes</h1>
         </header>
         <main className="flex-1 p-4">{children}</main>
       </div>
--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,17 @@
-import NextAuth from "next-auth";
-import { authConfig } from "@/auth";
+import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import { auth } from "@/auth";
 
-export default NextAuth(authConfig).auth;
+// export default NextAuth(authConfig).auth;
+export default async function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname === '/dashboard') {
+    const session = await auth();
+    if (!session) {
+      return NextResponse.redirect('/login');
+    }
+  }
+  return NextResponse.next();
+}
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)']
