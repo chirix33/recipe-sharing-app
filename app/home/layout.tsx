0e180f88ec16b9039f9603db85ba3bde26fa0bc5
chirix33 <chirixteam@gmail.com>
2024-11-06 23:28:13 -0600

    Display template Page for the Recipes end users would see at /home page route

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,11 +1,21 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
-    return (
-      // Nav for all the Recipe Pages
-      <div className="flex flex-col h-screen">
-        <header className="bg-blue-500 text-white p-4">
-          <h1 className="text-2xl">Recipes</h1>
-        </header>
-        <main className="flex-1 p-4">{children}</main>
-      </div>
-    );
+import { auth } from "@/auth";
+import Link from "next/link";
+import { UserCircleIcon, ArrowRightEndOnRectangleIcon } from "@heroicons/react/20/solid";
+
+export default async function Layout({ children }: { children: React.ReactNode }) {
+  const user = await auth();
+  return (
+    // Nav for all the Recipe Pages
+    <div className="flex flex-col">
+      <header className="flex justify-between bg-mallard-500 text-white p-4">
+        <h1 className="text-xl">Explore Recipes</h1>
+        {
+          user ? 
+          (<Link href="/dashboard" className="underline text-emerald-950 font-bold flex items-center justify-center gap-2"><UserCircleIcon className="w-6 h-6" /> My Account</Link>) : 
+          (<Link href="/login" className="underline text-emerald-950 font-bold flex items-center justify-center gap-2"><ArrowRightEndOnRectangleIcon className="w-6 h-6" /> Login / Sign Up</Link>)
+        }
+      </header>
+      <main className="flex-1">{children}</main>
+    </div>
+  );
 }
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,6 +1,29 @@
 export default function Page() {
     return (
         <>
+        <div className="mb-8 w-full h-28 bg-white-100 flex flex-col justify-center items-center p-4">
+            <h2>Search for Recipes</h2>
+        </div>
+        <div className="flex flex-col items-center">
+            {/* <h2 className="text-2xl mb-2">Featured Recipes</h2> */}
+            <div className="w-full p-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+                <div className="p-4 rounded-lg">
+                    <div className="w-full h-48 bg-zinc-400 mb-4"></div>
+                    <h3 className="text-xl">Recipe 1</h3>
+                    <p>Recipe description</p>
+                </div>
+                <div className="p-4 rounded-lg">
+                    <div className="w-full h-48 bg-zinc-400 mb-4"></div>
+                    <h3 className="text-xl">Recipe 2</h3>
+                    <p>Recipe description</p>
+                </div>
+                <div className="p-4 rounded-lg">
+                    <div className="w-full h-48 bg-zinc-400 mb-4"></div>
+                    <h3 className="text-xl">Recipe 3</h3>
+                    <p>Recipe description</p>
+                </div>
+            </div>
+        </div>
         </>
     );
 }
