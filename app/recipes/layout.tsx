6d8d71494efdbaf8a0040799bce4b89918ee95d6
chirix33 <chirixteam@gmail.com>
2024-11-15 15:37:10 -0600

    Removed ArrowRightEndOnRectangleIcon, updated navigation links and added wrapping Link component to 'Explore Recipes' header title

--- app/recipes/layout.tsx
+++ app/recipes/layout.tsx
@@ -1,6 +1,6 @@
 import { auth } from "@/auth";
 import Link from "next/link";
-import { UserCircleIcon, ArrowRightEndOnRectangleIcon } from "@heroicons/react/20/solid";
+import { UserCircleIcon } from "@heroicons/react/20/solid";
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
   const user = await auth();
@@ -8,11 +8,11 @@ export default async function Layout({ children }: { children: React.ReactNode }
     // Nav for all the Recipe Pages
     <div className="flex flex-col h-screen">
       <header className="flex justify-between bg-mallard-500 text-white p-4">
-        <h1 className="text-xl">Explore Recipes</h1>
+        <h1 className="text-xl"><Link href="/recipes">Explore Recipes</Link></h1>
         {
           user ? 
           (<Link href="/dashboard" className="text-emerald-950 font-bold flex items-center justify-center gap-2"><UserCircleIcon className="w-6 h-6" /> My Account</Link>) : 
-          (<Link href="/login" className="text-emerald-950 font-bold flex items-center justify-center gap-2"><ArrowRightEndOnRectangleIcon className="w-6 h-6" /> Login / Sign Up</Link>)
+          (<span className="text-emerald-950 font-bold flex items-center justify-center gap-2"><Link href="/login">Login</Link> | <Link href="/create">Sign Up</Link></span>)
         }
       </header>
       <main className="flex-1">{children}</main>
