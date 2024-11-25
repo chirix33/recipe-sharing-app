96e4513f1508b5daf6bf1c77264e32e040efa575
chirix33 <chirixteam@gmail.com>
2024-11-25 07:09:50 -0600

    Add login section to Home component with images and sign-in link

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { playwrite } from '@/app/ui/global/fonts';
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+    <div className="min-h-screen flex flex-col items-center justify-center">
       {/* Hero Section */}
       <div className="w-full bg-[url('/images/main_header_pic.jpg')] bg-cover bg-center">
         <div className="w-full! flex flex-col items-center justify-center px-6 py-16 text-center bg-slate-900/50 text-slate-50">
@@ -25,8 +25,24 @@ export default function Home() {
         </div>
       </div>
 
+      {/* Login Section */}
+      <div className="flex items-center justify-start w-full h-96 my-16 p-4">
+        <div className='flex-0 basis-1/4 h-full'>
+          <Image alt="Sandwich" width={640} height={960} src={`/images/sandwich.jpg`} className="object-contain h-full" />
+        </div>
+        <div className='flex-0 basis-1/4 h-full'>
+          <Image alt="Sandwich" width={360} height={514} src={`/images/squash_salad.jpg`} className="object-contain h-full" />
+        </div>
+        <div className="flex-1 relative text-center text-slate-700 h-full p-6 bg-gray-200">
+          <h3 className="text-xl"><span className={playwrite.className}>Taste</span> with an account</h3>
+          <p className="text-lg mb-4">Sign in to your account to save and share your favorite recipes.</p>
+          <Link href="/login" className="absolute bottom-4 left-2/4 -translate-x-1/2 inline-block bg-mallard-600 text-white-50 px-6 py-3 rounded-md text-lg font-medium hover:bg-white-50 hover:text-mallard-500 hover:border-mallard-500">
+              Sign In
+          </Link>
+        </div>
+      </div>
       {/* Features Section */}
-      <div className="container mx-auto px-6 pb-16">
+      {/* <div className="container mx-auto px-6 pb-16">
         <div className="flex flex-wrap -mx-4">
           <div className="w-full md:w-1/2 px-4 mb-8 md:mb-0">
             <div className="bg-white p-6 rounded-lg shadow-md text-center">
@@ -51,9 +67,9 @@ export default function Home() {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
 
-      {/* Call to Action Section */}
+      {/* Footer Section */}
       <div className="w-full bg-mallard-600 text-white-50 py-16">
         <div className="container mx-auto px-6 text-center">
           <h2 className="text-3xl font-bold mb-4">Join RecipeShare Today</h2>
