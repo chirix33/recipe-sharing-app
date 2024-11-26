d58f09c279cec18c392e036abcb213d92da4086c
chirix33 <chirixteam@gmail.com>
2024-11-26 11:57:13 -0600

    Add overflow-hidden class to main and login form for improved layout

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,7 +6,7 @@ import { FormSkeleton } from "@/app/ui/skeletons/formSkeletons";
 export default function Page() {
     return (
 
-        <main className="flex h-screen items-center justify-center md:h-screen">
+        <main className="flex h-screen items-center justify-center md:h-screen overflow-hidden">
             <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
                 <Logo className="text-xl font-semibold text-center" />
                 <Suspense fallback={<FormSkeleton />}>
